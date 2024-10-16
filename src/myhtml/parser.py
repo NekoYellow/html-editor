68ba057ae25ea3683956a35b03b142dbf0043ea4
Yueyuan Huang <harvyhyy@gmail.com>
2024-10-16 20:21:40 +0800

    Implement html parser to build tree

---
+++ src/myhtml/parser.py
@@ -0,0 +1,36 @@
+from html.parser import HTMLParser
+from node import HtmlNode
+
+
+class MyHtmlParser(HTMLParser):
+    def __init__(self):
+        super().__init__()
+        self.root = HtmlNode("", "")
+        self.stack = [self.root]
+        self.ids = set()
+    
+    def handle_starttag(self, tag, attrs):
+        print("Encountered a start tag:", tag)
+        key = tag
+        for k, v in attrs:
+            if k == "id":
+                key = v
+        self.ids.add(key)
+        curr = HtmlNode(tag, key)
+        self.stack[-1].add_children(curr)
+        self.stack.append(curr)
+
+    def handle_endtag(self, tag):
+        print("Encountered an end tag :", tag)
+        self.stack.pop()
+
+    def handle_data(self, data):
+        print("Encountered some data  :", data)
+        self.stack[-1].text = data
+    
+    def get_tree(self):
+        return self.root.children[0]
+
+    def get_ids(self):
+        return self.ids
+
