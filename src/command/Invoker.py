d50d57b9b066f64d93c8d1e4e7c507148aa8a188
Yueyuan Huang <harvyhyy@gmail.com>
2024-10-22 21:18:06 +0800

    Implement invoker

--- src/command/Invoker.py
+++ src/command/Invoker.py
@@ -1,2 +1,31 @@
 import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from command.command import Command, UndoableCommand
+
+class Invoker:
+    def __init__(self):
+        self.done = []
+        self.undone = []
+    
+    def store_and_execute(self, command):
+        command.execute()
+        if isinstance(command, UndoableCommand):
+            self.done.append(command)
+            self.undone.clear()
+    
+    def undo_last(self):
+        if self.done == []:
+            return
+        cmd = self.done.pop()
+        if not isinstance(cmd, UndoableCommand):
+            return
+        cmd.undo()
+        self.undone.append(cmd)
+        
+    def redo_last(self):
+        if self.undone == []:
+            return
+        cmd = self.undone.pop()
+        cmd.execute()
+        self.done.append(cmd)
