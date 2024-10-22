ce167ad735ba9179ac9e0b0d626cc159a7c5beac
Yueyuan Huang <harvyhyy@gmail.com>
2024-10-22 21:20:50 +0800

    Remove unnecessary import

--- src/command/Invoker.py
+++ src/command/Invoker.py
@@ -2,7 +2,6 @@ import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from command.command import Command, UndoableCommand
-from console.show_command import ShowCommand
 
 class Invoker:
     def __init__(self):
