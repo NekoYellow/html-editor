b2c6b2e257fda0debb3be1fc51de20d2a540d75c
Yueyuan Huang <harvyhyy@gmail.com>
2024-10-22 20:06:37 +0800

    Implement printing html tree as a file

--- src/myhtml/tree.py
+++ src/myhtml/tree.py
@@ -5,6 +5,7 @@ import os, sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from util.treeshow import TreeShow
+from util.visitor import Visitor
 
 
 class HtmlTree:
@@ -40,8 +41,33 @@ class HtmlTree:
     
     def show_as_tree(self):
         TreeShow(self.root).print()
+    
+    def show_as_text(self):
+        tv = TextVisitor()
+        tv.visit(self.root)
+        print(tv.get_text())
 
 
+class TextVisitor(Visitor):
+    def __init__(self):
+        self.s = ""
 
-ht = HtmlTree("../sample.html")
+    def visit(self, node, indent=0, is_last=True, mask=0):
+        self.s += "  " * indent
+        if node.is_text:
+            self.s += node.tag + "\n"
+        else:
+            self.s += "<" + node.tag + (" id=\"" + node.id + "\"" if node.id != None else "") + ">\n"
+        for child in node.children:
+            self.visit(child, indent + 1)
+        if not node.is_text:
+            self.s += "  " * indent
+            self.s += "<\\" + node.tag + ">\n"
+    
+    def get_text(self):
+        return self.s
+
+
+ht = HtmlTree("sample.html")
 ht.show_as_tree()
+ht.show_as_text()
